2ad301e9d1ab240082f013a44a9b581dde8d7214
mohamed-ahmed-el-nagar2003 <mohamedahmed50201@gmail.com>
2026-02-20 02:59:10 +0200

    Add project files.

---
+++ OOP_Encapsolation_Assignment_02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Encapsolation_Assignment_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
